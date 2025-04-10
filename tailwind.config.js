d06b550008cc385b5f20e88c8626192d7ba7ae9a
reforco-dev <reforcodev.atendimento@gmail.com>
2025-04-10 18:49:58 -0300

    inicio de projeto

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'vinho': '#64221D',
+                'mostarda': '#F7AE1A',
+                'creme': '#F5E7DA',
+                'caramelo': '#CF9A5A',
+                'bege': '#A97744',
+                'canela': '#C7794A',
+                'marrom': '#3B2416',
+                'cafe': '#292625',
+            }
+        },
+    },
+    plugins: [],
+}
