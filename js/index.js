5e714b0c0aa236cde5a64a0e71e8c60ef821d128
FranciscoJose28 <fcojmatos28@gmail.com>
2025-06-02 22:57:08 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -60,23 +60,36 @@ function quemSomos(direcao) {
     // console.log(opcao);
 }
 
-function diferenciais(iconGrande){
+function diferenciais(iconGrande,opcao){
     const iconMaior = document.querySelector('#iconMaior');
+    const titulo = document.querySelector('#titulo-diferenciais')
     const text = document.querySelector('#texto-diferenciais');
     iconMaior.src = iconGrande;
 
     if (opcao == 1) {
-        icon.src = "./images/diferencial1-grande.png";
-        text.innerText = "A tiamate possui faturamento bruto percentual de lucro de 12% a 18%";
+        iconMaior.src = "./images/diferencial1-grande.png";
+        titulo.innerText = "Media de Lucratividade"
+        text.innerText = "A tiamate possui faturamento bruto percentual de lucro de 12% a 18%.";
     } else if (opcao == 2) {
-        icon.src = "./images/diferencial2G.png";
+        iconMaior.src = "./images/diferencial2G.png";
+        titulo.innerText = "Operação e Logística"
         text.innerText = "A Tiamate possui um departamento de logistica que avalia cada caso e analisa a entrega oferecendo soluções para que as unidades não fiquem sem produtos.";
     } else if (opcao == 3) {
-        icon.src = "./images/diferencial3G.png";
-        text.innerText = "Possui um software de gestão integrada";
+        iconMaior.src = "./images/diferencial3G.png";
+        titulo.innerText = "Software"
+        text.innerText = "Possui um software de gestão integrada.";
     } else if (opcao == 4) {
-        icon.src = "./images/diferencial4G.png";
+        iconMaior.src = "./images/diferencial4G.png";
+        titulo.innerText = "Analise de Ponto Comercial"
         text.innerText = "Dedica profissionais para prospecção e análise de pontos que sejam pertinentes ao modelo de negócios.";
+    } else if (opcao == 5) {
+        iconMaior.src = "./images/diferencial5G.png";
+        titulo.innerText = "Equipe"
+        text.innerText = "A Tiamate possui um departamento que auxilia na seleção e contratação da 1ª equipe.";
+    } else if (opcao == 6) {
+        iconMaior.src = "./images/diferencial6G.png";
+        titulo.innerText = "Desenvolvimento de Produtos"
+        text.innerText = "Possui uma equipe interdiciplinar de Nutricionistas e baristas que desenvolvem e se preocupam com os melhores padrões de sabor e nutrição dos produtos.";
     }
 }
 
